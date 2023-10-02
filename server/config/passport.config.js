195cd5321967bb5d5554f295c79e499943fdd073
potocki92 <mateusz.potocki92@gmail.com>
2023-10-02 22:08:13 +0200

    server/config
    Added Passport JWT middleware for user authentication, which
    utilizes a secret key defined in the configuration file .env.

---
+++ server/config/passport.config.js
@@ -0,0 +1,27 @@
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const User = require("../models/user.model");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const Strategy = passportJWT.Strategy;
+const params = {
+  secretOrKey: SECRET_KEY,
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+};
+
+passport.use(
+  new Strategy(params, function (payload, done) {
+    User.find({ _id: payload.id })
+      .then(([user]) => {
+        if (!user) {
+          return done(new Error("User not found"));
+        }
+        return done(null, user);
+      })
+      .catch((err) => done(err));
+  })
+);
