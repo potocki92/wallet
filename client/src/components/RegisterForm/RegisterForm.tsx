9c209b6ff8294a5de5ed34b4c99c7ac78960d675
potocki92 <mateusz.potocki92@gmail.com>
2023-10-13 13:06:56 +0200

    components/RegisterForm
    Created RegisterForm Component
    
    This commit adds a new component called 'RegisterForm'. It is designed for user registration and utilizes the 'InputsForm' component. The 'RegisterForm' component simply renders the 'InputsForm' component with the 'Register' button text and sets the 'formType' prop to 'register'. This allows users to fill in their email and password for registration.
    
    The 'RegisterForm' component can be easily incorporated into the registration page of the application, providing a clean and consistent form for user registration.

---
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,11 @@
+import InputsForm from "../InputsForm/InputsForm"
+
+const RegisterForm = () => {
+  return (
+    <>
+      <InputsForm buttonText="Register" formType="register"/>
+    </>
+  );
+};
+
+export default RegisterForm;
