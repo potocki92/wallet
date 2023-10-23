7d7807edbb3b2081108cc6371ec5093d9a48b6fd
potocki92 <mateusz.potocki92@gmail.com>
2023-10-23 12:30:56 +0200

    components/LoginForm
    Added LoginForm component
    
    In this commit, a new component called "LoginForm" has been created. The LoginForm component utilizes the "InputsForm" component, which provides input fields for email and password, as well as a submit button.
    
    The "LoginForm" component sets the "buttonText" prop to "Login" and the "formType" prop to "login." This allows it to reuse the input form structure while customizing the button text and form type for login purposes.
    
    This component will be used in the login page of the application, making it easier to maintain and reuse the input form structure for different authentication forms.

---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import InputsForm from "../InputsForm/InputsForm"
+
+const LoginForm = () => {
+  return (
+    <>
+      <InputsForm buttonText="Login" formType="login"/>
+    </>
+  );
+};
+
+export default LoginForm;
