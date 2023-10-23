53ba81e3ac598892759e4f91eadf940c3bb428ab
potocki92 <mateusz.potocki92@gmail.com>
2023-10-23 12:21:45 +0200

    auth/authSlice
    Added authentication slice
    
    This commit introduces an authentication slice to the project using Redux Toolkit. The slice contains initial state, reducers for successful registration and login, and additional properties to manage user authentication, including user data, token, login status, and refresh status.

---
+++ client/src/hooks/useAuth.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectUser,
+} from "../api/auth/selectors";
+
+export const useAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+  return {
+    isLoggedIn,
+    isRefreshing,
+    user,
+  };
+};
--- client/src/hooks/useAuth.tsx
+++
@@ -1,5 +0,0 @@
-const useAuth = () => {
-
-}
-
-export default useAuth
