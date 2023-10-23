cfacb6fbf248ac8bb98c6f2366d55fb6326f4a35
potocki92 <mateusz.potocki92@gmail.com>
2023-10-23 12:24:08 +0200

    auth/operations
    Added user registration and login async thunks
    
    This commit includes the creation of two asynchronous thunks for user registration and login. These thunks use axios to make API requests to the server and handle responses and errors accordingly. The setAuthHeader function is used to set the Bearer token in Axios headers upon successful login, and clearAuthHeader is used to remove the token from the headers. These thunks are integrated into the authSlice for state management.

---
+++ client/src/api/auth/operations.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000/";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post("/api/users/signup", credentials);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post("api/users/signin", credentials);
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
