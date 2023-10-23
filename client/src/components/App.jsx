01f85b7de5aa8d63451c16172ef397e4180f7fe4
potocki92 <mateusz.potocki92@gmail.com>
2023-10-23 12:57:49 +0200

    components/App
    Improved route management with RestrictedRoute and PrivateRoute
    
    In this commit, route management in the application has been improved. Two new components, "RestrictedRoute" and "PrivateRoute," have been introduced to handle route access.
    
    - "RestrictedRoute" is designed to handle routes that are restricted to unauthenticated users. It redirects unauthenticated users to a specified destination, such as the login page, and prevents access to certain routes.
    - "PrivateRoute" is used for routes that should only be accessible to authenticated users. It ensures that only authenticated users can access specific pages and redirects unauthenticated users to a designated location, such as the login page.
    
    The existing routes have been updated to use these new components, enhancing the overall security and accessibility of different parts of the application.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,42 @@
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Suspense, lazy, useEffect } from "react";
-import Home from "../pages/Home";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { PrivateRoute } from "./PrivateRoute";
+
+const HomePage = lazy(() => import("../pages/Home"));
+const RegisterPage = lazy(() => import("../pages/Register"));
+const LoginPage = lazy(() => import("../pages/Login"));
 
 export const App = () => {
-
   return (
     <>
-    
-    <Router>
-        <Suspense fallback={<div>Loading...</div>}></Suspense>
-        <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-        </Routes>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              path="*"
+              element={
+                <RestrictedRoute
+                  redirectTo="/home"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute redirectTo="/home" component={<LoginPage />} />
+              }
+            />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute redirectTo="/login" component={<HomePage />} />
+              }
+            />
+          </Routes>
+        </Suspense>
       </Router>
     </>
   );
