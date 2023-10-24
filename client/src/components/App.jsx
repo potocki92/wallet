8e8237f0969ec3a69fa4b89dfb8c6e3480c9b696
potocki92 <mateusz.potocki92@gmail.com>
2023-10-24 12:09:15 +0200

    refactor/App
    Refactor App Component and Implement User Refresh
    
    This commit refactors the App component and introduces user refresh functionality:
    
    - Import `useDispatch` from 'react-redux' to enable Redux dispatch.
    - Utilize `useDispatch` and the `refreshUser` action to refresh the user's data.
    - Implement a conditional rendering approach based on `isRefreshing` from the 'useAuth' hook.
    - While refreshing user data, display a "Refreshing user..." message.
    - Otherwise, render the app within the 'Router' component with proper routing.
    
    These changes enhance the App component with user data refreshing and conditional rendering, providing a smoother user experience.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,14 +2,25 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Suspense, lazy, useEffect } from "react";
 import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivateRoute";
+import { useDispatch } from "react-redux/es";
+import { useAuth } from "../hooks/useAuth";
+import { refreshUser } from "../api/auth/operations";
 
 const HomePage = lazy(() => import("../pages/Home"));
 const RegisterPage = lazy(() => import("../pages/Register"));
 const LoginPage = lazy(() => import("../pages/Login"));
 
 export const App = () => {
-  return (
-    <>
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <div>
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
@@ -38,6 +49,6 @@ export const App = () => {
           </Routes>
         </Suspense>
       </Router>
-    </>
+    </div>
   );
 };
