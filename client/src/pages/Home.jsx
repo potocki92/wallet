85ed99f544af0f0336347bd01f8be753522f7f6c
potocki92 <mateusz.potocki92@gmail.com>
2023-10-24 12:11:06 +0200

    update/Home
    Update Home Component to Use Redux Selectors
    
    This commit updates the Home component to use Redux selectors for accessing user data and login status.
    
    Changes made:
    - Import 'useSelector' from 'react-redux'.
    - Use 'selectUser' and 'selectIsLoggedIn' selectors to retrieve user data and login status.
    - Removed console.log used for debugging.
    
    The Home component now correctly utilizes Redux selectors for obtaining user data and login status, improving the application's data management.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn, selectUser } from "../api/auth/selectors";
+
 const Home = () => {
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  console.log(user, isLoggedIn);
   return <>Home</>;
 };
 
