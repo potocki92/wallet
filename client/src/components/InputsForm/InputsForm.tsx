d097347badcf1e29adc651fd91e227c68f25e7ac
potocki92 <mateusz.potocki92@gmail.com>
2023-11-07 13:32:40 +0100

    remove/InputsForm
    UseDispatch and useSelector has been removed.

--- client/src/components/InputsForm/InputsForm.tsx
+++ client/src/components/InputsForm/InputsForm.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { signIn, register } from "../../api/auth/operations";
 import { selectIsLoggedIn, selectUser } from "../../api/auth/selectors";
