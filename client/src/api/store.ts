c71e633987387e2f3dbf793137d41f7519794318
potocki92 <mateusz.potocki92@gmail.com>
2023-11-07 12:30:03 +0100

    update/store
    Optimize Authentication State Persistence Configuration
    
    This commit optimizes the configuration for persisting the authentication state in the application.
    
    Changes made:
    - Created a 'authPersistConfig' object to specify the key, storage method, and whitelist for the authentication state.
    - Utilized 'persistReducer' to apply the configuration to the 'authReducer'.
    - Updated the store configuration to include both the original 'authReducer' and the 'persistedAuthReducer'.
    
    These changes improve the way the authentication state is stored and ensure that only the necessary data is persisted, enhancing the application's performance.

--- client/src/api/store.ts
+++ client/src/api/store.ts
@@ -1,11 +1,23 @@
+import { persistStore } from 'redux-persist';
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const authPersistConfig = {
+  key: "auth", // Klucz, pod którym zostanie zachowany stan autoryzacji
+  storage, // Wybrana metoda przechowywania (np. localStorage)
+  whitelist: ["token"], // Lista kluczy stanu autoryzacji do zachowania
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
