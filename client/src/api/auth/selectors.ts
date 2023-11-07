2ba5a350e7321a6658f3f38cffb8cf4201fffbcf
potocki92 <mateusz.potocki92@gmail.com>
2023-11-07 12:29:40 +0100

    update/selectors
    Remove selectToken Selector
    
    This commit removes the 'selectToken' selector as it is no longer needed in the application.
    
    Changes made:
    - Removed the 'selectToken' selector function.
    
    The removal of 'selectToken' is in line with the changes made to the codebase and ensures a more streamlined and focused selector setup.

--- client/src/api/auth/selectors.ts
+++ client/src/api/auth/selectors.ts
@@ -2,5 +2,4 @@ import { RootState } from "../store";
 
 export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
 export const selectUser = (state: RootState) => state.auth.user;
-export const selectToken = (state: RootState) => state.auth.token;
 export const selectIsRefreshing = (state: RootState) => state.auth.isRefreshing;
