313edeb9c7ec4c101a782279bd8a4544eebcd618
potocki92 <mateusz.potocki92@gmail.com>
2023-11-07 12:30:40 +0100

    update/index
    Update Application Rendering Configuration
    
    This commit updates the configuration and rendering of the application for improved performance.
    
    Changes made:
    - Removed the duplicate import of the 'store' from "./api/store".
    - Utilized the 'createRoot' method to create the application root and render it.
    - Wrapped the 'Provider' component with 'PersistGate' to ensure that the Redux state is hydrated before rendering the 'App' component.
    
    These changes optimize the application's rendering process, eliminating redundancy and ensuring proper state hydration.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./api/store";
+import { persistor, store } from "./api/store";
 import { App } from "./components/App";
+import { PersistGate } from "redux-persist/integration/react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
