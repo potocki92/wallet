2f3b76f39d655cd41b90bfc3b1158a2a9136adba
potocki92 <mateusz.potocki92@gmail.com>
2023-11-09 12:59:28 +0100

    added/Home
    Add: Introduce temporary button in Home component.
    
    Enhanced the Home component by adding a temporary button to facilitate testing or interaction with the logOut functionality. This button, when clicked, dispatches the logOut action. Useful for temporary debugging or development purposes.
    
    Changes:
    - Added a temporary button in the Home component.
    - Configured the button to dispatch the logOut action on click.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectUser } from "../api/auth/selectors";
+import { useAppDispatch } from "../api/hooks";
+import { logOut } from "../api/auth/operations";
 
 const Home = () => {
+  const dispatch = useAppDispatch()
   const user = useSelector(selectUser);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   console.log(user, isLoggedIn);
-  return <>Home</>;
+  return <>Home
+  <button onClick={()=> dispatch(logOut())}></button></>;
 };
 
 export default Home;
