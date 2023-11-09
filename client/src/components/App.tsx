5b26aedcd32535ffb4fb64d177b3841e17e278e9
potocki92 <mateusz.potocki92@gmail.com>
2023-11-09 12:55:39 +0100

    refactor/App
    Refactor: Streamline routing configuration in App component.
    
    Updated the routing configuration in the App component for better organization and readability. Removed unnecessary code duplication and simplified the structure using shared layout components. The routing now uses a more modular approach, enhancing maintainability.
    
    Changes:
    - Consolidated the routing logic in the App component.
    - Replaced individual Route elements with nested routes and shared layout components.
    - Updated the structure to improve code readability and reduce duplication.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,51 +2,46 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Suspense, lazy, useEffect } from "react";
 import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivateRoute";
-import { useAuth } from "../hooks/useAuth";
 import { refreshUser } from "../api/auth/operations";
 import { useAppDispatch } from "../api/hooks";
-import Register from "../pages/Register";
+import SharedLayoutRestricted from "./SharedLayoutRestricted";
+import SharedLayoutPrivate from "./SharedLayoutPrivate";
 
 const HomePage = lazy(() => import("../pages/Home"));
 const RegisterPage = lazy(() => import("../pages/Register"));
 const LoginPage = lazy(() => import("../pages/Login"));
-const AuthenticationPage = lazy(() => import("../pages/Authentication"));
 
 export const App = () => {
   const dispatch = useAppDispatch();
-  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
-  return isRefreshing ? (
-    <b>Refreshing user...</b>
-  ) : (
+  return (
     <div>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
+        <Routes>
+          <Route path="/" element={<SharedLayoutRestricted />}>
             <Route
-              path="*"
-              element={
-                <RestrictedRoute redirectTo="/home" component={RegisterPage} />
-              }
-            />
-            <Route
-              path="/login"
+              index
               element={
                 <RestrictedRoute redirectTo="/home" component={LoginPage} />
               }
             />
-            <Route path="/register" element={<RegisterPage />} />
             <Route
-              path="/home"
+              path="register"
               element={
-                <PrivateRoute redirectTo="/login" component={HomePage} />
+                <RestrictedRoute redirectTo="/home" component={RegisterPage} />
               }
             />
-          </Routes>
-        </Suspense>
+          </Route>
+          <Route path="/home" element={<SharedLayoutPrivate />}>
+            <Route
+              index
+              element={<PrivateRoute redirectTo="/" component={HomePage} />}
+            />
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
